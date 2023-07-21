b32346ce1e29e72bef492e5007048d9efd978f20
AJAX <22mapbs019@ddu.ac.in>
2023-07-21 13:07:53 +0530

    Prime

---
+++ OneLiner.js
@@ -0,0 +1,13 @@
+let Num = 153;
+
+/*===Prime Number Checker===*/
+const Prime = (Num, i = Math.floor(Num / 2)) => (Num % i == 0) ? "Not a Prime Number " : (i == 2) ? "Prime Number" : Prime(Num, i - 1);
+
+/*===Palindrom===*/
+const Palindrom = (Num) => Num.toString().split("").reverse().join("") == Num ? "Palindrom" : "Not a Palindrom";
+
+/*===Fibonacci===*/
+const Fibonacci = (Num) => Num <= 1 ? Num : Fibonacci(Num - 1) + Fibonacci(Num - 2);
+
+/*===Perfect Number===*/
+const Perfect = (Num) => Array.from({ length: Num - 1 }, (_, i) => Num % (i + 1) === 0 ? i + 1 : 0).reduce((a, b) => a + b, 0) === Num ? "Perfect Number" : "Not a Perfect Number";
