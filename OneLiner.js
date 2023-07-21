ddfb701d8ccc54240c223470a8b696f541052cdb
AJAX <22mapbs019@ddu.ac.in>
2023-07-21 13:10:45 +0530

    EvenOdd

--- OneLiner.js
+++ OneLiner.js
@@ -1,5 +1,8 @@
 let Num = 153;
 
+/*===Even Odd Number Checker===*/
+const EvenOdd = (Num) => (Num % 2 == 0) ? "Even Number" : "Odd Number";
+
 /*===Prime Number Checker===*/
 const Prime = (Num, i = Math.floor(Num / 2)) => (Num % i == 0) ? "Not a Prime Number " : (i == 2) ? "Prime Number" : Prime(Num, i - 1);
 
