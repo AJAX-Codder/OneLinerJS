7156c0705ab82836cb14686277b1d3e67704f1a0
AJAX <22mapbs019@ddu.ac.in>
2023-08-14 18:01:23 +0530

    MaxMin

--- OneLiner.js
+++ OneLiner.js
@@ -58,10 +58,15 @@ var isPalindrome = (s) => s.replace(/[^0-9a-zA-Z]/g, "").replaceAll(" ", "").toL
  */
 const lengthOfLastWord = s => s.trim().split(" ").reverse()[0].length
 
-/*[14].
+/**[14].
     Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
 
     For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
     Return the resulting string.
 */
 const reversePrefix = (word, ch) => word.indexOf(ch) == -1 ? word : word.substring(0, word.indexOf(ch) + 1).split("").reverse().join("") + word.substring(word.indexOf(ch) + 1)
+
+/*[15].
+    find Max sum excluding minimum element and Min sum excluding maximum element
+*/
+const miniMaxSum = (arr) => console.log(arr.sort((a, b) => a - b).reduce((a, b) => a + b) - arr[arr.length - 1], arr.reduce((a, b) => a + b) - arr[0])
